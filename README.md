d683d50134bee8ab398390037c7c061894ee732b
Ismail ‎ <ellouismail@gmail.com>
2022-05-20 14:24:16 +0100

    Added the folder structure and set up the index.js

---
+++ README.md
@@ -0,0 +1,2 @@
+# tasty_recipes
+Tasty Recipes is a webapp for finding detailed recipes information, saving them in your account, or sharing them on social media. 
---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config()
+
+const PORT = process.env.PORT || 4000;  
+const CONNECTION_URL = `${process.env.CONNECTION_URL}` 
+
+//to properly send reqs
+app.use(bodyParser.json({limit : '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit : '30mb', extended: true}));
+
+app.use(cors());
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => app.listen(PORT, () => console.log(`Server listening on ${PORT}`) ))
+.catch(error => console.error(error))
